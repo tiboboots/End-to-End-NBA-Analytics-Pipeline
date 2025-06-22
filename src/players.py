e1c8e5829eb3e64e1787e1f72df0ea857f1b4412
Tibo <tiboboots@outlook.com>
2025-06-22 12:38:40 +0200

    Simplified the references to the PlayerCareerStats class in the Player class

--- src/players.py
+++ src/players.py
@@ -24,6 +24,6 @@ class Player:
             print(f"KeyError: {k}")
             self.player_id = None
 
-    def player_career_stats(self, per_mode: str = "PerGame") -> ep.playercareerstats.PlayerCareerStats:
-        player_stats = ep.playercareerstats.PlayerCareerStats(player_id=self.player_id, per_mode36=per_mode)
+    def player_career_stats(self, per_mode: str = "PerGame") -> ep.PlayerCareerStats:
+        player_stats = ep.PlayerCareerStats(player_id=self.player_id, per_mode36=per_mode)
         return player_stats
