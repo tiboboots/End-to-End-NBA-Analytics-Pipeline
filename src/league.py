a0fdcc5615ebce4fa9c9fc5eed5f876bf2ede6cb
Tibo <tiboboots@outlook.com>
2025-06-24 12:47:09 +0200

    Added keyword arguments to the rest of the league methods in the NBA class

--- src/league.py
+++ src/league.py
@@ -10,25 +10,25 @@ class NBA:
         return ep.LeagueDashTeamStats(season=season, season_segment_nullable=season_type_all_star, **kwargs)
     
     @staticmethod
-    def league_team_shot_location_stats(**kwargs):
-        return ep.LeagueDashTeamShotLocations(**kwargs)
+    def league_team_shot_location_stats(season: str, season_type_all_star: str = "Regular Season", **kwargs):
+        return ep.LeagueDashTeamShotLocations(season=season, season_type_all_star=season_type_all_star, **kwargs)
     
     @staticmethod
-    def league_player_shot_location_stats(**kwargs):
-        return ep.LeagueDashPlayerShotLocations(**kwargs)
+    def league_player_shot_location_stats(season: str, season_type_all_star: str="Regular Season", **kwargs):
+        return ep.LeagueDashPlayerShotLocations(season=season, season_type_all_star=season_type_all_star, **kwargs)
     
     @staticmethod
-    def league_hustle_stats_player(**kwargs):
-        return ep.LeagueHustleStatsPlayer(**kwargs)
+    def league_hustle_stats_player(season: str, season_type_all_star: str="Regular Season", **kwargs):
+        return ep.LeagueHustleStatsPlayer(season=season, season_type_all_star=season_type_all_star, **kwargs)
     
     @staticmethod
-    def league_hustle_stats_team(**kwargs):
-        return ep.LeagueHustleStatsTeam(**kwargs)
+    def league_hustle_stats_team(season: str, season_type_all_star: str="Regular Season", **kwargs):
+        return ep.LeagueHustleStatsTeam(season=season, season_type_all_star=season_type_all_star, **kwargs)
     
     @staticmethod
     def league_standings(season: str):
         return ep.LeagueStandingsV3(season=season)
     
     @staticmethod
-    def league_clutch_player_stats(**kwargs):
-        return ep.LeagueDashPlayerClutch(**kwargs)
+    def league_clutch_player_stats(season: str, season_type_all_star: str="Regular Season", **kwargs):
+        return ep.LeagueDashPlayerClutch(season=season, season_type_all_star=season_type_all_star, **kwargs)
