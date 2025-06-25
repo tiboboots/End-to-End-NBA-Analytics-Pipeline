838ba4bdc11d7998b22a53e8decc41bfbbc7b1f0
Tibo <tiboboots@outlook.com>
2025-06-25 10:00:21 +0200

    Got rid of all methods in the nba class, as these can all be calculated ourselves from raw data

--- src/league.py
+++ src/league.py
@@ -1,37 +1,4 @@
 import nba_api.stats.endpoints as ep
 
 class NBA:
-    @staticmethod
-    def league_player_stats(season: str, season_type_all_star: str = "Regular Season", **kwargs):
-        return ep.LeagueDashPlayerStats(season=season, season_type_all_star=season_type_all_star, **kwargs)
-    
-    @staticmethod
-    def league_team_stats(season: str, season_type_all_star: str = "Regular Season", **kwargs):
-        return ep.LeagueDashTeamStats(season=season, season_segment_nullable=season_type_all_star, **kwargs)
-    
-    @staticmethod
-    def league_team_shot_location_stats(season: str, season_type_all_star: str = "Regular Season", **kwargs):
-        return ep.LeagueDashTeamShotLocations(season=season, season_type_all_star=season_type_all_star, **kwargs)
-    
-    @staticmethod
-    def league_player_shot_location_stats(season: str, season_type_all_star: str="Regular Season", **kwargs):
-        return ep.LeagueDashPlayerShotLocations(season=season, season_type_all_star=season_type_all_star, **kwargs)
-    
-    @staticmethod
-    def league_hustle_stats_player(season: str, season_type_all_star: str="Regular Season", **kwargs):
-        return ep.LeagueHustleStatsPlayer(season=season, season_type_all_star=season_type_all_star, **kwargs)
-    
-    @staticmethod
-    def league_hustle_stats_team(season: str, season_type_all_star: str="Regular Season", **kwargs):
-        return ep.LeagueHustleStatsTeam(season=season, season_type_all_star=season_type_all_star, **kwargs)
-    
-    @staticmethod
-    def league_standings(season: str):
-        return ep.LeagueStandingsV3(season=season)
-    
-    @staticmethod
-    def league_clutch_player_stats(season: str, ahead_behind: str = "Behind or Tied",
-                                   clutch_time: str = "Last 5 Minutes", season_type_all_star: str="Regular Season", 
-                                   **kwargs):
-        return ep.LeagueDashPlayerClutch(season=season, ahead_behind=ahead_behind, clutch_time=clutch_time,
-                                         season_type_all_star=season_type_all_star, **kwargs)
+    pass
