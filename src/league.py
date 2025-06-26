45774a2c3ede51f85cfb35e737fe1fcf49f2b9e8
Tibo <tiboboots@outlook.com>
2025-06-26 12:53:40 +0200

    Added methods to wrap the player and league shot location endpoints to the NBA class, also added instance attributes to ensure that every object contains league data for a specific season

--- src/league.py
+++ src/league.py
@@ -1,4 +1,21 @@
 import nba_api.stats.endpoints as ep
+from dataclasses import dataclass, field
 
+@dataclass
 class NBA:
-    pass
+    season: str
+    season_type_all_star: str
+
+    def __post_init__(self):
+        valid_season_types = {"Regular Season", "Playoffs"}
+        if self.season_type_all_star.title() not in valid_season_types:
+            raise ValueError(f"Season type is invalid. Must be one of: {valid_season_types}")
+
+    def league_team_shot_locations(self):
+        return ep.LeagueDashTeamShotLocations(season=self.season, 
+                                              season_type_all_star=self.season_type_all_star)
+    
+    def league_player_shot_locations(self):
+        return ep.LeagueDashPlayerShotLocations(season=self.season, 
+                                                season_type_all_star=self.season_type_all_star)
+    
