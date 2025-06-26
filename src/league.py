fe8b674e05a849a0957fa267541157f80f291163
Tibo <tiboboots@outlook.com>
2025-06-26 16:36:05 +0200

    Added the title method for formatting to the top of the post init method to ensure that the season type attribute is always properly formatted

--- src/league.py
+++ src/league.py
@@ -7,8 +7,9 @@ class NBA:
     season_type_all_star: str
 
     def __post_init__(self):
+        self.season_type_all_star = self.season_type_all_star.title()
         valid_season_types = {"Regular Season", "Playoffs"}
-        if self.season_type_all_star.title() not in valid_season_types:
+        if self.season_type_all_star not in valid_season_types:
             raise ValueError(f"Season type is invalid. Must be one of: {valid_season_types}")
         if len(self.season) != 4 or not self.season.isdigit():
             raise ValueError(f"Invalid season format: {self.season}. Format should be YYYY.")
@@ -20,4 +21,3 @@ class NBA:
     def league_player_shot_locations(self):
         return ep.LeagueDashPlayerShotLocations(season=self.season, 
                                                 season_type_all_star=self.season_type_all_star)
-    
