a76ff49992a5da0dda7de4494605a98334e20371
Tibo <tiboboots@outlook.com>
2025-06-26 16:31:07 +0200

    Added additional conditional check to validate the season format, we fail fast if format is invalid, like with the season_type

--- src/league.py
+++ src/league.py
@@ -10,7 +10,9 @@ class NBA:
         valid_season_types = {"Regular Season", "Playoffs"}
         if self.season_type_all_star.title() not in valid_season_types:
             raise ValueError(f"Season type is invalid. Must be one of: {valid_season_types}")
-
+        if len(self.season) != 4 or not self.season.isdigit():
+            raise ValueError(f"Invalid season format: {self.season}. Format should be YYYY.")
+        
     def league_team_shot_locations(self):
         return ep.LeagueDashTeamShotLocations(season=self.season, 
                                               season_type_all_star=self.season_type_all_star)
