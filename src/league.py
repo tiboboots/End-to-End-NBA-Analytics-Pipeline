f70c86cf877925e730f1859b1e0c994dcc41ff1b
Tibo <tiboboots@outlook.com>
2025-06-30 16:22:27 +0200

    Removed all validation and conditional checks from the NBA class as this is already handled by the nba_api package

--- src/league.py
+++ src/league.py
@@ -5,14 +5,6 @@ from dataclasses import dataclass, field
 class NBA:
     season: str
     season_type_all_star: str
-
-    def __post_init__(self):
-        self.season_type_all_star = self.season_type_all_star.title()
-        valid_season_types = {"Regular Season", "Playoffs"}
-        if self.season_type_all_star not in valid_season_types:
-            raise ValueError(f"Season type is invalid. Must be one of: {valid_season_types}")
-        if len(self.season) != 4 or not self.season.isdigit():
-            raise ValueError(f"Invalid season format: {self.season}. Format should be YYYY.")
         
     def league_team_shot_locations(self):
         return ep.LeagueDashTeamShotLocations(season=self.season, 
