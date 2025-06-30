e477ffb86f9cee2ac42da94cab766da06fb51a93
Tibo <tiboboots@outlook.com>
2025-06-30 17:06:30 +0200

    Created the box_score method to fetch the box score for a specific game using the game_id

--- src/league.py
+++ src/league.py
@@ -1,5 +1,5 @@
 import nba_api.stats.endpoints as ep
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 @dataclass
 class NBA:
@@ -16,3 +16,6 @@ class NBA:
     
     def league_game_stats(self):
         return ep.LeagueGameLog(season=self.season, season_type_all_star=self.season_type_all_star)
+    
+    def box_score(self, game_id: int):
+        return ep.BoxScoreTraditionalV3(game_id=game_id)
