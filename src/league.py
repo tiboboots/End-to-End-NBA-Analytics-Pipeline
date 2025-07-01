08298681c274037efdd507ede8ca3cee9a3bbc5e
Tibo <tiboboots@outlook.com>
2025-07-01 18:56:11 +0200

    Created the game_finder method to filter the leaguegamelog dataframe and return an updated version where the home team, away team, and game date match the values passed to the class when creating an instance. Also created a post_init method for standardizing the home team and away team strings

--- src/league.py
+++ src/league.py
@@ -1,5 +1,6 @@
 import nba_api.stats.endpoints as ep
 from dataclasses import dataclass, field
+import pandas as pd
 
 @dataclass
 class NBA:
@@ -23,12 +24,24 @@ class NBA:
 @dataclass
 class Game:
     game_id: int = field(init=False)
-    game_date: str # YYYY-MM-DD
+    game_date: str = None # YYYY-MM-DD
     home_team_name_short: str
     away_team_name_short: str
     season: str
     season_type_all_star: str = "Regular Season"
 
+    def __post_init__(self):
+        self.home_team_name_short = self.home_team_name_short.strip().upper()
+        self.away_team_name_short = self.away_team_name_short.strip().upper()
+
+    def game_finder(self):
+        df = ep.LeagueGameLog(season=self.season, 
+                                     season_type_all_star=self.season_type_all_star).get_data_frames()[0]
+
+        filtered_games = df[df['MATCHUP'].str.contains(self.home_team_name_short) 
+                            & df['MATCHUP'].str.contains(self.away_team_name_short)
+                            & df['GAME_DATE'] == self.game_date]
+    
     def box_score(self):
         return ep.BoxScoreTraditionalV3(game_id=self.game_id)
     
