410b2c6d5a696c4fbea7c084bf033a9185a1324f
Tibo <tiboboots@outlook.com>
2025-07-02 17:56:13 +0200

    Removed the get_all_team_ids method and the post init method from the Team class, as we will be restructuing the flow of the pipeline

--- src/teams.py
+++ src/teams.py
@@ -7,23 +7,6 @@ class Team:
     full_team_name: str
     team_id: int = field(init=False)
 
-    def get_all_team_ids(self) -> dict:
-        all_nba_teams = teams.get_teams()
-        all_team_ids = {}
-        for team in all_nba_teams:
-            team_id = team['id']
-            team_name = team['full_name']
-            all_team_ids[team_name] = team_id
-        return all_team_ids
-    
-    def __post_init__(self):
-        self.full_team_name = self.full_team_name.strip().title()
-        all_team_ids = self.get_all_team_ids()
-        if self.full_team_name in all_team_ids:
-            self.team_id = all_team_ids[self.full_team_name]
-        else:
-            raise KeyError(f"Team ID could not be located using team name: {self.full_team_name}")
-
     def team_roster(self, season: str):
         return ep.CommonTeamRoster(team_id= self.team_id, season=season)
     
