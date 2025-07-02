1e188336a1716e638a720b5fae5102b080027aa9
Tibo <tiboboots@outlook.com>
2025-07-02 18:00:29 +0200

    Refactored and cleaned up the Player class, similar changes as the ones to the other classes

--- src/players.py
+++ src/players.py
@@ -5,24 +5,9 @@ import nba_api.stats.endpoints as ep
 @dataclass
 class Player:
     player_full_name: str
-    player_id: int = field(init=False)
-
-    def get_all_player_ids(self) -> dict:
-        all_nba_players = players.get_players()
-        all_player_ids = {}
-        for player in all_nba_players:
-            player_id = player['id']
-            player_name = player['full_name']
-            all_player_ids[player_name] = player_id
-        return all_player_ids
-
-    def __post_init__(self):
-        self.player_full_name = self.player_full_name.strip().title()
-        all_player_ids = self.get_all_player_ids()
-        if self.player_full_name in all_player_ids:
-            self.player_id = all_player_ids[self.player_full_name]
-        else:
-            raise KeyError(f"Could not retrieve player id for {self.player_full_name}")
+    player_id: int
+    season: str
+    season_type_all_star: str
     
-    def player_game_stats(self, season: str, season_type_all_star: str = "Regular Season"):
-        return ep.PlayerGameLog(player_id=self.player_id, season=season, season_type_all_star=season_type_all_star)
+    def player_game_stats(self):
+        return ep.PlayerGameLog(player_id=self.player_id, season=self.season, season_type_all_star=self.season_type_all_star)
