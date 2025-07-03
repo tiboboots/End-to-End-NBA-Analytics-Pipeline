19bf91f21853965bfe1bcfe965fbce0889fc9b57
Tibo <tiboboots@outlook.com>
2025-07-03 11:32:27 +0200

    Created the extract_player_games and extract_team_games function in the extract module, to extract all player and team game logs across a list of seasons, convert them to a dataframe, combine them into a single dataframe, and return the combined dataframe

---
+++ src/extract.py
@@ -0,0 +1,32 @@
+import logging
+import nba_api.stats.endpoints as ep
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+def extract_player_games(seasons: list):
+    dataframes = []
+    for season in seasons:
+        try:
+            season_player_games = ep.LeagueGameLog(season=season, 
+                                            player_or_team_abbreviation="P").get_normalized_dict()["LeagueGameLog"]
+            df_player_games = pd.DataFrame(data=season_player_games)
+            dataframes.append(df_player_games)
+        except Exception as e:
+            logger.exception(f"Error trying to extract and convert game logs to dataframes: {e}")
+            raise
+    return pd.concat(objs=dataframes, ignore_index=True)
+
+
+def extract_team_games(seasons: list):
+    dataframes = []
+    try:
+        for season in seasons:
+            season_team_games = ep.LeagueGameLog(season=season, 
+                                        player_or_team_abbreviation="T").get_normalized_dict()["LeagueGameLog"]
+            df_team_games = pd.DataFrame(data=season_team_games)
+            dataframes.append(df_team_games)
+    except Exception as e:
+        logging.exception(f"Error trying to extract and convert team game logs to dataframes: {e}")
+        raise
+    return pd.concat(objs=dataframes, ignore_index=True)
