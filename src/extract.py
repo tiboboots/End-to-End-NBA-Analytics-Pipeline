cb174627ae30e7c3d120ae174a9793f11100569d
Tibo <tiboboots@outlook.com>
2025-07-03 12:27:00 +0200

    Created the extract_team_roster method which gets the roster for a team across a list of seasons, converts the list of dictionaries returned into a dataframe, then combines the dataframes for each season into 1 dataframe

--- src/extract.py
+++ src/extract.py
@@ -9,12 +9,15 @@ def extract_player_games(seasons: list) -> pd.DataFrame:
     for season in seasons:
         try:
             season_player_games = ep.LeagueGameLog(season=season, 
-                                            player_or_team_abbreviation="P").get_normalized_dict()["LeagueGameLog"]
+            player_or_team_abbreviation="P").get_normalized_dict()["LeagueGameLog"]
+
             df_player_games = pd.DataFrame(data=season_player_games)
             dataframes.append(df_player_games)
+
         except Exception as e:
             logger.exception(f"Error trying to extract and convert game logs to dataframes: {e}")
             raise
+
     return pd.concat(objs=dataframes, ignore_index=True)
 
 def extract_team_games(seasons: list) -> pd.DataFrame:
@@ -22,10 +25,29 @@ def extract_team_games(seasons: list) -> pd.DataFrame:
     try:
         for season in seasons:
             season_team_games = ep.LeagueGameLog(season=season, 
-                                        player_or_team_abbreviation="T").get_normalized_dict()["LeagueGameLog"]
+            player_or_team_abbreviation="T").get_normalized_dict()["LeagueGameLog"]
+
             df_team_games = pd.DataFrame(data=season_team_games)
             dataframes.append(df_team_games)
+
     except Exception as e:
         logging.exception(f"Error trying to extract and convert team game logs to dataframes: {e}")
         raise
+
+    return pd.concat(objs=dataframes, ignore_index=True)
+
+def extract_team_roster(seasons: list, team_id: int):
+    dataframes = []
+    try:
+        for season in seasons:
+            season_team_roster = ep.CommonTeamRoster(team_id=team_id, 
+            season=season).get_normalized_dict()["CommonTeamRoster"]
+            
+            df = pd.DataFrame(data=season_team_roster)
+            dataframes.append(df)
+
+    except Exception as e:
+        logging.exception(f"Error occured trying to extract and convert team roster to dataframe: {e}")
+        raise
+    
     return pd.concat(objs=dataframes, ignore_index=True)
