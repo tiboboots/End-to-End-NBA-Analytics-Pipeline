3fbabf63c26fe4c55adf2b19865f70d4b1677bf2
Tibo <tiboboots@outlook.com>
2025-07-03 12:48:51 +0200

    Combined the extract_player_games and extract_team_games methods into a single extract_game_logs function, as we can use the player_or_team_abbreviation to filter by player or team logs dynamically

--- src/extract.py
+++ src/extract.py
@@ -4,35 +4,21 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-def extract_player_games(seasons: list) -> pd.DataFrame:
+def extract_game_logs(seasons: list, player_or_team_abbreviation: str, 
+                      season_type_all_star: str) -> pd.DataFrame:
     dataframes = []
     for season in seasons:
         try:
             season_player_games = ep.LeagueGameLog(season=season, 
-            player_or_team_abbreviation="P").get_normalized_dict()["LeagueGameLog"]
+            player_or_team_abbreviation=player_or_team_abbreviation,
+            season_type_all_star=season_type_all_star).get_normalized_dict()["LeagueGameLog"]
 
-            df_player_games = pd.DataFrame(data=season_player_games)
-            dataframes.append(df_player_games)
+            df = pd.DataFrame(data=season_player_games)
+            dataframes.append(df)
 
         except Exception as e:
             logger.exception(f"Error trying to extract and convert game logs to dataframes: {e}")
             raise
-
-    return pd.concat(objs=dataframes, ignore_index=True)
-
-def extract_team_games(seasons: list) -> pd.DataFrame:
-    dataframes = []
-    try:
-        for season in seasons:
-            season_team_games = ep.LeagueGameLog(season=season, 
-            player_or_team_abbreviation="T").get_normalized_dict()["LeagueGameLog"]
-
-            df_team_games = pd.DataFrame(data=season_team_games)
-            dataframes.append(df_team_games)
-
-    except Exception as e:
-        logging.exception(f"Error trying to extract and convert team game logs to dataframes: {e}")
-        raise
 
     return pd.concat(objs=dataframes, ignore_index=True)
 
