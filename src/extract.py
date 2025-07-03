3d7a38c1ecbf8929a44a99ff095c56be53c43fa8
Tibo <tiboboots@outlook.com>
2025-07-03 16:34:30 +0200

    Removed the try except blocks from the extract game logs and extract team roster functions, as they dont help much, and we are better off letting the error naturally be raised by Python

--- src/extract.py
+++ src/extract.py
@@ -7,40 +7,33 @@ logger = logging.getLogger(__name__)
 def extract_game_logs(seasons: list, player_or_team_abbreviation: str, 
                       season_type_all_star: str) -> pd.DataFrame:
     dataframes = []
+
     for season in seasons:
-        try:
-            season_player_games = ep.LeagueGameLog(season=season, 
-            player_or_team_abbreviation=player_or_team_abbreviation,
-            season_type_all_star=season_type_all_star).get_normalized_dict()["LeagueGameLog"]
+        season_player_games = ep.LeagueGameLog(season=season, 
+        player_or_team_abbreviation=player_or_team_abbreviation,
+        season_type_all_star=season_type_all_star).get_normalized_dict()["LeagueGameLog"]
 
-            df = pd.DataFrame(data=season_player_games)
-            dataframes.append(df)
-
-        except Exception as e:
-            logger.exception(f"Error trying to extract and convert game logs to dataframes: {e}")
-            raise
+        df = pd.DataFrame(data=season_player_games)
+        dataframes.append(df)
 
     return pd.concat(objs=dataframes, ignore_index=True)
 
 def extract_team_roster(seasons: list, team_id: int) -> pd.DataFrame:
     dataframes = []
-    try:
-        for season in seasons:
-            season_team_roster = ep.CommonTeamRoster(team_id=team_id, 
-            season=season).get_normalized_dict()["CommonTeamRoster"]
-            
-            df = pd.DataFrame(data=season_team_roster)
-            dataframes.append(df)
 
-    except Exception as e:
-        logging.exception(f"Error occured trying to extract and convert team roster to dataframe: {e}")
-        raise
+    for season in seasons:
+        season_team_roster = ep.CommonTeamRoster(team_id=team_id, 
+        season=season).get_normalized_dict()["CommonTeamRoster"]
+        
+        df = pd.DataFrame(data=season_team_roster)
+        dataframes.append(df)
     
     return pd.concat(objs=dataframes, ignore_index=True)
 
 def extract_shot_locations(seasons: list, player_or_team: str, season_type_all_star: str) -> pd.DataFrame:
     player_or_team = player_or_team.lower()
     dataframes = []
+
     for season in seasons:
         if player_or_team == "player":
             league_player_shots = ep.LeagueDashPlayerShotLocations(season=season, 
