ceb923343f1bc7cc8a388053d8cbe9b6c281d0ba
Tibo <tiboboots@outlook.com>
2025-07-05 23:50:44 +0200

    Removed transformation logic from the extract_shot_locations function in favor of returning only the raw multi-index dataframe, and moving the transformation to the transform module

--- src/extract.py
+++ src/extract.py
@@ -44,29 +44,5 @@ def extract_shot_locations(season: str, player_or_team: str,
     else:
         logger.error(f"Incorrect value for player_or_team parameter in extract_shot_locations", exc_info= True)
         raise ValueError
-    
-    shot_zones = []
-    metadata = []
 
-    for tup in df.columns: 
-        if len(tup[0]) > 0:
-            shot_location = "_".join(tup)
-            shot_zones.append(shot_location) 
-        else:
-            metadata.append(tup[1])
-
-    df.columns = metadata + shot_zones 
-
-    df_long = pd.melt(frame=df, id_vars=metadata, var_name="shot_location", value_name="value") 
-
-    df_long = df_long[df_long['shot_location'].str.contains("FGA") | df_long["shot_location"].str.contains("FGM")]
-
-    df_long[['shot_location', "shot_type"]] = df_long['shot_location'].str.split("_", n=1, expand=True)
-
-    df_wide = df_long.pivot(index = metadata + ['shot_location'], 
-                            columns='shot_type', 
-                            values='value').reset_index()
-
-    df_wide.columns.name = None
-
-    return df_wide
+    return df
