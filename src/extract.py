33b42b27af870ef98b71c6b998cc5fc21d21f476
Tibo <tiboboots@outlook.com>
2025-07-06 11:35:11 +0200

    Removed the for loops from the extract_game_logs and extract_team_roster functions as well, as we want all of our functions to only return a single dataframe per season

--- src/extract.py
+++ src/extract.py
@@ -4,31 +4,25 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-def extract_game_logs(seasons: list, player_or_team_abbreviation: str, 
+def extract_game_logs(season: str, player_or_team_abbreviation: str, 
                       season_type_all_star: str) -> pd.DataFrame:
-    dataframes = []
 
-    for season in seasons:
-        season_player_games = ep.LeagueGameLog(season=season, 
-        player_or_team_abbreviation=player_or_team_abbreviation,
-        season_type_all_star=season_type_all_star).get_normalized_dict()["LeagueGameLog"]
+    season_player_games = ep.LeagueGameLog(season=season, 
+    player_or_team_abbreviation=player_or_team_abbreviation,
+    season_type_all_star=season_type_all_star).get_normalized_dict()["LeagueGameLog"]
 
-        df = pd.DataFrame(data=season_player_games)
-        dataframes.append(df)
+    df = pd.DataFrame(data=season_player_games)
 
-    return pd.concat(objs=dataframes, ignore_index=True)
+    return df
 
-def extract_team_roster(seasons: list, team_id: int) -> pd.DataFrame:
-    dataframes = []
-
-    for season in seasons:
-        season_team_roster = ep.CommonTeamRoster(team_id=team_id, 
-        season=season).get_normalized_dict()["CommonTeamRoster"]
+def extract_team_roster(season: str, team_id: int) -> pd.DataFrame:
         
-        df = pd.DataFrame(data=season_team_roster)
-        dataframes.append(df)
+    season_team_roster = ep.CommonTeamRoster(team_id=team_id, 
+    season=season).get_normalized_dict()["CommonTeamRoster"]
+        
+    df = pd.DataFrame(data=season_team_roster)
     
-    return pd.concat(objs=dataframes, ignore_index=True)
+    return df
 
 def extract_shot_locations(season: str, player_or_team: str, 
                            season_type_all_star: str = "Regular Season") -> pd.DataFrame:
