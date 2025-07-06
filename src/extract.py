a78e9e3ef3ed15a842735d2da11862920c8a1e11
Tibo <tiboboots@outlook.com>
2025-07-06 13:18:41 +0200

    Updated type hints in all extract functions

--- src/extract.py
+++ src/extract.py
@@ -4,17 +4,17 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-def extract_game_logs(season: str, player_or_team_abbreviation: str, season_type_all_star: str) -> pd.DataFrame:
+def extract_game_logs(season: str, player_or_team_abbreviation: str, season_type_all_star: str) -> dict:
     return ep.LeagueGameLog(season=season, player_or_team_abbreviation=player_or_team_abbreviation,
     season_type_all_star=season_type_all_star).get_dict()
 
 
-def extract_team_roster(season: str, team_id: int) -> pd.DataFrame:
+def extract_team_roster(season: str, team_id: int) -> dict:
     return ep.CommonTeamRoster(team_id=team_id, season=season).get_dict()
 
 
 def extract_shot_locations(season: str, player_or_team: str, 
-                           season_type_all_star: str = "Regular Season") -> pd.DataFrame:
+                           season_type_all_star: str = "Regular Season") -> dict:
     
     if player_or_team.lower() == "player":
         return ep.LeagueDashPlayerShotLocations(season=season, season_type_all_star=season_type_all_star).get_dict()
@@ -27,7 +27,7 @@ def extract_shot_locations(season: str, player_or_team: str,
         raise ValueError
     
 
-def extract_box_scores(game_id: str, box_score_type: str):
+def extract_box_scores(game_id: str, box_score_type: str) -> dict:
 
     endpoints = {"traditional": ep.BoxScoreTraditionalV3,
                  "matchups": ep.BoxScoreMatchupsV3,
