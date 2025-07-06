2459e5ab7f938f857421aa584f2c2f0ac4b11b36
Tibo <tiboboots@outlook.com>
2025-07-06 20:33:54 +0200

    Created initial new version of the transform_shot_locations function, now meant to transform the raw dictionary into a dataframe, additional functionality and logic will be added later on

--- src/transform.py
+++ src/transform.py
@@ -1,28 +1,15 @@
 import pandas as pd
 
-def transform_shot_locations(df: pd.DataFrame):
-    shot_zones = []
-    metadata = []
+def transform_shot_locations(shot_locations: dict):
+    columns_and_rows = {}
 
-    for tup in df.columns: 
-        if len(tup[0]) > 0:
-            shot_location = "_".join(tup)
-            shot_zones.append(shot_location) 
-        else:
-            metadata.append(tup[1])
+    result_columns = shot_locations['resultSets']['headers']
 
-    df.columns = metadata + shot_zones 
+    columns = []
+    for dict in result_columns:
+        columns.extend(dict['columnNames'])
 
-    df_long = pd.melt(frame=df, id_vars=metadata, var_name="shot_location", value_name="value") 
+    columns_and_rows['columns'] = columns
+    columns_and_rows['rows'] = shot_locations['resultSets']['rowSet']
 
-    df_long = df_long[df_long['shot_location'].str.contains("FGA") | df_long["shot_location"].str.contains("FGM")]
-
-    df_long[['shot_location', "shot_type"]] = df_long['shot_location'].str.split("_", n=1, expand=True)
-
-    df_wide = df_long.pivot(index = metadata + ['shot_location'], 
-                            columns='shot_type', 
-                            values='value').reset_index()
-
-    df_wide.columns.name = None
-
-    return df_wide
+    return columns_and_rows
