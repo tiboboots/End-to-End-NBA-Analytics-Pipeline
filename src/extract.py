9b809b0ab953ff431f41722a11fbe2f29e323926
Tibo <tiboboots@outlook.com>
2025-07-06 13:16:44 +0200

    Added direct returns for the extract_shot_locations function

--- src/extract.py
+++ src/extract.py
@@ -17,18 +17,15 @@ def extract_shot_locations(season: str, player_or_team: str,
                            season_type_all_star: str = "Regular Season") -> pd.DataFrame:
     
     if player_or_team.lower() == "player":
-        response = ep.LeagueDashPlayerShotLocations(season=season, 
-        season_type_all_star=season_type_all_star).get_dict()
+        return ep.LeagueDashPlayerShotLocations(season=season, season_type_all_star=season_type_all_star).get_dict()
     
     elif player_or_team.lower() == "team":
-        response = ep.LeagueDashTeamShotLocations(season=season,
-        season_type_all_star=season_type_all_star).get_dict()
+        return ep.LeagueDashTeamShotLocations(season=season,season_type_all_star=season_type_all_star).get_dict()
 
     else:
         logger.error(f"Incorrect value for player_or_team parameter in extract_shot_locations", exc_info= True)
         raise ValueError
-
-    return response
+    
 
 def extract_box_scores(game_id: str, box_score_type: str):
 
