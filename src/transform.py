066dcb9f3a394fdcf0310d67331d6950737be21c
Tibo <tiboboots@outlook.com>
2025-07-08 16:19:46 +0200

    Added try except block with logging to the transform_shot_locations orchestrator function

--- src/transform.py
+++ src/transform.py
@@ -1,8 +1,21 @@
 import helper_functions_transform as hp
+import logging
+
+logger = logging.getLogger(__name__)
 
 def transform_shot_locations(shot_locations: dict):
-    df = hp.shotzones_df(shot_locations=shot_locations)
-    player_data_columns, df_flat = hp.shotzones_flat_df(shot_locations=shot_locations, df=df)
-    shot_locations_df = hp.shotzones_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
+    try:
+        logger.info(f"Extracting shot location header list and converting to exploded dataframe")
+        df = hp.shotzones_df(shot_locations=shot_locations)
 
-    return shot_locations_df
+        logger.info(f"Converting shot locations to a flat dataframe")
+        player_data_columns, df_flat = hp.shotzones_flat_df(shot_locations=shot_locations, df=df)
+
+        logger.info(f"Pivoting flat shot locations dataframe to wide version")
+        shot_locations_df = hp.shotzones_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
+
+        return shot_locations_df
+    
+    except Exception as e:
+        logger.exception(f"Error trying to extract and convert shot locations to wide dataframe: {e}")
+        raise
