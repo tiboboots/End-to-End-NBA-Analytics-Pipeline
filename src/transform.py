c46a0e13db462a3a671fdf9004d53e05270fd219
Tibo <tiboboots@outlook.com>
2025-07-08 12:27:56 +0200

    Turned transform_shot_locations function into an orchestrator function, calling multiple helper functions to transform the shot_locations data for players or teams

--- src/transform.py
+++ src/transform.py
@@ -1,15 +1,8 @@
-import pandas as pd
+import helper_functions_transform as hp
 
 def transform_shot_locations(shot_locations: dict):
-    columns_and_rows = {}
+    df = hp.shotzones_df(shot_locations=shot_locations)
+    player_data_columns, df_flat = hp.shotzones_flat_df(shot_locations=shot_locations, df=df)
+    shot_locations_df = hp.shotzones_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
 
-    result_columns = shot_locations['resultSets']['headers']
-
-    columns = []
-    for dict in result_columns:
-        columns.extend(dict['columnNames'])
-
-    columns_and_rows['columns'] = columns
-    columns_and_rows['rows'] = shot_locations['resultSets']['rowSet']
-
-    return columns_and_rows
+    return shot_locations_df
