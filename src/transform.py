e1b97b20b2cfe704ce1297a0f33686b553f130e8
Tibo <tiboboots@outlook.com>
2025-07-08 19:10:27 +0200

    Got rid of the try/except block and logging in the transform_shot_locations function in favor of using the log decorator function, which achieves the same thing but in a more concise and clean way

--- src/transform.py
+++ src/transform.py
@@ -1,21 +1,15 @@
 import helper_functions_transform as hp
 import logging
+from decorators import log
 
 logger = logging.getLogger(__name__)
 
+@log
 def transform_shot_locations(shot_locations: dict):
-    try:
-        logger.info(f"Extracting shot location header list and converting to exploded dataframe")
-        df = hp.shotzones_df(shot_locations=shot_locations)
+    df = hp.shotzones_df(shot_locations=shot_locations)
 
-        logger.info(f"Converting shot locations to a flat dataframe")
-        player_data_columns, df_flat = hp.shotzones_flat_df(shot_locations=shot_locations, df=df)
+    player_data_columns, df_flat = hp.shotzones_flat_df(shot_locations=shot_locations, df=df)
 
-        logger.info(f"Pivoting flat shot locations dataframe to wide version")
-        shot_locations_df = hp.shotzones_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
+    shot_locations_df = hp.shotzones_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
 
-        return shot_locations_df
-    
-    except Exception as e:
-        logger.exception(f"Error trying to extract and convert shot locations to wide dataframe: {e}")
-        raise
+    return shot_locations_df
