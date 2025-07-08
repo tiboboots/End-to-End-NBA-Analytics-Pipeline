ce29d65ef1106f00dda5fd03de9c988cc2a5e703
Tibo <tiboboots@outlook.com>
2025-07-08 20:05:05 +0200

    Refactored @log decorator function to handle edge cases where a function returns a tuple with a dataframe in it, also added line breaks after every log for cleaner logs

--- src/decorators.py
+++ src/decorators.py
@@ -8,18 +8,28 @@ def log(func):
     @functools.wraps(func)
     def wrapper_func(*args, **kwargs):
         try:
-            logger.info(f"Running {func.__name__}")
+            logger.info(f"Running {func.__name__}\n")
             output = func(*args, **kwargs)
 
             if isinstance(output, pd.DataFrame):
-                logger.debug(f"{func.__name__} result: {output.columns} {output.shape}")
+                logger.debug(f"{func.__name__} result: Columns = {output.columns}, Shape = {output.shape}\n")
+            
+            elif isinstance(output, tuple):
+                logger.info(f"{func.__name__} returns a tuple. Now showing various items returned in the tuple. \n")
+                item_count = 0
+                for item in output:
+                    item_count += 1
+                    if isinstance(item, pd.DataFrame):
+                        logger.debug(f"{func.__name__} item number {item_count}: Columns = {item.columns}, Shape = {item.shape}\n")
+                    else:
+                        logger.debug(f"{func.__name__} item number {item_count}: {item}\n")
             else:
-                logger.debug(f"{func.__name__} result: {output}")
+                logger.debug(f"{func.__name__} result: {output}\n")
                 
             return output
         
         except Exception as e:
-            logger.error(f"Error in {func.__name__}: {e}")
+            logger.error(f"Error in {func.__name__}: {e}\n")
             raise
 
     return wrapper_func
