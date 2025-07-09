452f9f89d4639cbe7eefad0781efd3fcc3d97161
Tibo <tiboboots@outlook.com>
2025-07-09 09:31:19 +0200

    Renamed the helper_functions_transform module to shot_location_helpers, as we want to group helper functions by endpoints

--- src/transform.py
+++ src/transform.py
@@ -1,4 +1,4 @@
-import helper_functions_transform as hp
+import shot_location_helpers as slh
 import logging
 from decorators import log
 
@@ -6,10 +6,10 @@ logger = logging.getLogger(__name__)
 
 
 def transform_shot_locations(shot_locations: dict):
-    df = hp.shotlocations_to_df(shot_locations=shot_locations)
+    df = slh.shotlocations_to_df(shot_locations=shot_locations)
 
-    player_data_columns, df_flat = hp.shotlocations_combine_columns(shot_locations=shot_locations, df=df)
+    player_data_columns, df_flat = slh.shotlocations_combine_columns(shot_locations=shot_locations, df=df)
 
-    shot_locations_df = hp.shotlocations_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
+    shot_locations_df = slh.shotlocations_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
 
     return shot_locations_df
