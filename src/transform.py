6cf703e63c6a6d020eb61b3e402c9e1ce83baba1
Tibo <tiboboots@outlook.com>
2025-07-09 12:23:17 +0200

    Created the transform_game_logs function which take raw extracted game logs and converts it to a dataframe, then does any necessary filtering and cleaning before returning it

--- src/transform.py
+++ src/transform.py
@@ -1,6 +1,7 @@
 import shot_location_helpers as slh
 import logging
 from decorators import log
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +14,17 @@ def transform_shot_locations(shot_locations: dict):
     shot_locations_df = slh.shotlocations_pivot_df(player_data_columns=player_data_columns, df_flat=df_flat)
 
     return shot_locations_df
+
+@log()
+def transform_game_logs(game_logs: dict):
+    game_log_columns = game_logs['resultSets'][0]['headers']
+    game_log_rows = game_logs['resultSets'][0]['rowSet']
+
+    df = pd.DataFrame(data=game_log_rows, columns=game_log_columns)
+
+    cols_to_drop = [col for col in df.columns if 'PCT' in col or col in ['FANTASY_PTS', 'VIDEO_AVAILABLE']]
+
+    if len(cols_to_drop) > 0:
+        df = df.drop(cols_to_drop, axis=1)
+
+    return df
