b3b4e132125a0e3b8423d14e51362c23b9959b35
Tibo <tiboboots@outlook.com>
2025-07-13 15:55:40 +0200

    Deleted extract_box_scores function as getting box scores for multiple games was not feasible due to unreliable server for batch requests

--- src/extract.py
+++ src/extract.py
@@ -34,20 +34,3 @@ def extract_shot_locations(season: str, player_or_team: str,
     else:
         logger.error(f"Incorrect value for player_or_team parameter in extract_shot_locations", exc_info= True)
         raise ValueError
-    
-    
-@log()
-def extract_box_scores(game_id: str, box_score_type: str) -> dict:
-
-    endpoints = {"traditional": ep.BoxScoreTraditionalV3,
-                 "matchups": ep.BoxScoreMatchupsV3,
-                 "hustle": ep.BoxScoreHustleV2,
-                 "defense": ep.BoxScoreDefensiveV2,
-                 "misc": ep.BoxScoreMiscV3}
-    
-    if box_score_type.lower() in endpoints:
-        return endpoints[box_score_type](game_id=game_id).get_dict()
-
-    else:
-        logger.error(f"Invalid value provided for the box_score_type parameter: {box_score_type}", exc_info=True)
-        raise ValueError 
